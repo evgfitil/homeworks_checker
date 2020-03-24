8e2bf0e60ae330f7b12b1902f69d3478057396d2
Evgenii Akhmetzyanov <evgfitil@yandex.ru>
2020-03-24 16:46:49 +0500

    First commit

--- homework.py
+++ homework.py
@@ -13,8 +13,8 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,14 +22,24 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    params = {'from_date': current_timestamp}
+
+    homework_statuses = requests.get(
+        url,
+        headers=headers,
+        params=params
+    )
+    
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    proxy = telegram.utils.request.Request(proxy_url='socks5://134.209.100.103:49616')
+    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
+
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
