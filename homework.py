bdbd8bd7413405e98c58471a5c10d2f84b0b4e08
Evgenii Akhmetzyanov <evgfitil@yandex.ru>
2020-03-26 11:04:17 +0500

    After first code review

--- homework.py
+++ homework.py
@@ -1,15 +1,22 @@
 import os
+import time
+
 import requests
 import telegram
-import time
+
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+proxy_url = 'socks5://134.209.100.103:49616'
+proxy = telegram.utils.request.Request(proxy_url=proxy_url)
+bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
 
 
 def parse_homework_status(homework):
@@ -22,41 +29,57 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     params = {'from_date': current_timestamp}
 
-    homework_statuses = requests.get(
-        url,
-        headers=headers,
-        params=params
-    )
+    try:
+        homework_statuses = requests.get(
+            url,
+            headers=headers,
+            params=params
+        )
     
+    except requests.exceptions.RequestException as e:
+        raise bot_interrupt(e)
+    
+    if homework_statuses.json().get('source'):
+        e = homework_statuses.json().get('message')
+        raise bot_interrupt(e)
+
     return homework_statuses.json()
 
 
 def send_message(message):
-    proxy = telegram.utils.request.Request(proxy_url='socks5://134.209.100.103:49616')
-    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
 
-    return bot.send_message(chat_id=CHAT_ID, text=message)
+    return bot.send_message(
+        chat_id=CHAT_ID, 
+        text=message,
+        connection_timeout=10,
+        read_timeout=10
+    )
+
+
+def bot_interrupt(e):
+    response = f'Работа бота остановлена. Ошибка: {e}'
+    return SystemExit(response)
 
 
 def main():
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
+        
         try:
-            new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            all_homeworks = get_homework_statuses(current_timestamp)
+            new_homework = all_homeworks.get('homeworks')[0]
+            if new_homework:
+                send_message(parse_homework_status(new_homework))
+            current_timestamp = new_homework.get('current_date')
+            time.sleep(300)
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
-            time.sleep(5)
-            continue
+            raise bot_interrupt(e)
 
 
 if __name__ == '__main__':
